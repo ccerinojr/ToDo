88a61105b9b520ff5be180e4942a344afb6d3467
Carmen Cerino <ccerinojr@gmail.com>
2013-02-21 13:44:32 -0500

    Initial Commit

---
+++ ToDo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDo
+//
+//  Created by Cerino, Carmen on 2/21/13.
+//  Copyright (c) 2013 TechSmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ToDo
+//
+//  Created by Cerino, Carmen on 2/21/13.
+//  Copyright (c) 2013 TechSmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
